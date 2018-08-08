210111a97eb6417496d3d55bd18da1ac05d6b2f8
szymon-datalions <s.molinski@datalions.eu>
2018-08-08 22:36:24 +0200

    debugged setupy.py and tutorials

--- pyinterpolate/__init__.py
+++ pyinterpolate/__init__.py
@@ -1,6 +1,1 @@
-from .kriging import covariance
-from .kriging import semivariance
-from .kriging import fit_semivariance
-from .kriging import kriging
-
 __version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Project description
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 try:
