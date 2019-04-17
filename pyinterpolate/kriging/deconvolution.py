corrected areal semivariance calculation

pyinterpolate/kriging/deconvolution.py
@@ -15,22 +15,22 @@ from pyinterpolate.kriging.helper_functions.euclidean_distance import centroid_b
 def calculate_semivariogram_deviation(data_based_semivariogram, theoretically_regularized_semivariogram):
     """Function calculates deviation between experimental and theoretical semivariogram
     over given lags.
-    
+
     INPUT:
     :param data_based_semivariogram: data based semivariogram in the form of numpy array:
     [[lag 0, value 0],
      [lag i, value i],
      [lag z, value z]]
-    
+
     :param theoretically_regularized_semivariogram: array in the same for as data_based_semivariogram,
     where first column represents the same lags as the first column of the data_based_semivariogram array.
-    
+
     OUTPUT:
     :return deviation: scalar which describes deviation between semivariograms.
      """
-    
+
     array_length = len(data_based_semivariogram)
-    
+
     if array_length == len(theoretically_regularized_semivariogram):
         if (data_based_semivariogram[:, 0] == theoretically_regularized_semivariogram[:, 0]).any():
             print('Start of deviation calculation')
@@ -49,8 +49,9 @@ class DeconvolutedModel:
     """Class is a pipeline for deconvolution of areal data"""
 
     def __init__(self):
-        
+
         # Models
+        self.base_point_support = None
         self.experimental_point_support = None
         self.initial_point_support_model = None
         self.optimal_point_support_model = None
@@ -64,77 +65,94 @@ class DeconvolutedModel:
         self.difference_statistics_ratio = 1
         self.difference_statistics_limit = 0.01
         self.difference_statistics_decrease_number = 3
-        
+
         # Areal Semivariance models
         self.within_block_semivariogram = None
         self.semivariogram_between_blocks = None
-        
+
         # Additional
         self.centroid_distances = None
         self.areal_distances = None
 
-
     def regularize(self, areal_data_file, areal_lags, areal_step_size, data_column,
-                 population_data_file, population_value_column, population_lags, population_step_size,
-                 id_column_name):
+                   population_data_file, population_value_column, population_lags, population_step_size,
+                   id_column_name):
 
-        areal_semivariance = ArealSemivariance(areal_data_file, areal_lags, areal_step_size, data_column,
-                 population_data_file, population_value_column, population_lags, population_step_size,
-                 id_column_name)
-        
         # Point support model
         centroids_semivar = Semivariance(areal_data_file, lags=areal_lags,
-                                          step_size=areal_step_size,
-                                          id_field=id_column_name)
+                                         step_size=areal_step_size,
+                                         id_field=id_column_name)
         self.experimental_point_support = centroids_semivar.centroids_semivariance(data_column='LB RATES 2')
         self.centroid_distances = centroid_blocks_distances(centroids_semivar.g_dict)
-        
-        # Initial and optimal point support models
-        
-        theoretical_model = TheoreticalSemivariogram(centroids_semivar.centroids,
-                                                    self.experimental_point_support)
 
-        theoretical_model.find_optimal_model(weighted=False, number_of_ranges=100)
-        
+        # Initial and optimal point support models
+
+        theoretical_model = TheoreticalSemivariogram(centroids_semivar.centroids,
+                                                     self.experimental_point_support)
+
+        theoretical_model.find_optimal_model(weighted=True, number_of_ranges=100)
+
         self.optimal_point_support_model = theoretical_model
-        
+        values_from_base = theoretical_model.calculate_values()
+        self.base_point_support = values_from_base
+
+        # Areal
+
+        areal_semivariance = ArealSemivariance(self.optimal_point_support_model,
+                                                areal_data_file, areal_lags, areal_step_size, data_column,
+                                                population_data_file, population_value_column, population_lags,
+                                                population_step_size, id_column_name)
+
         # Regularized Model
         regularized = areal_semivariance.blocks_semivariance()
         self.areal_distances = areal_semivariance.areal_distances
         self.within_block_semivariogram = areal_semivariance.within_block_semivariogram
         self.semivariogram_between_blocks = areal_semivariance.semivariogram_between_blocks
         self.optimal_regularized_model = regularized
-        
+
         # Deviation
         deviation = calculate_semivariogram_deviation(self.experimental_point_support, regularized)
         self.optimal_difference_statistics = deviation
-        
+
         return self.optimal_point_support_model, self.optimal_regularized_model
-    
-    
+
     # Data visualization methods
-    
+
     def show_distances(self, centroids=None, weights=None):
-        
+
         if not centroids:
             x = pd.DataFrame.from_dict(self.centroid_distances, orient='index')
             x.sort_index(inplace=True)
             x.sort_index(axis=1, inplace=True)
             x = (x.values).ravel()
-            centroids = np.where(x>0, np.log(x), 0)
+            centroids = np.where(x > 0, np.log(x), 0)
             centroids[centroids == 0] = np.nan
-            
+
         if not weights:
             y = pd.DataFrame.from_dict(self.areal_distances, orient='index')
             y.sort_index(inplace=True)
             y.sort_index(axis=1, inplace=True)
             y = (y.values).ravel()
-            weights = np.where(y>0, np.log(y), 0)
+            weights = np.where(y > 0, np.log(y), 0)
             weights[weights == 0] = np.nan
-        
+
         plt.figure(figsize=(12, 12))
         plt.scatter(x=centroids, y=weights, s=3, alpha=0.8)
         plt.title("Population-weighted distances")
         plt.xlabel("Log10 (block distance)")
         plt.ylabel("Log10 (Euclidean distance)")
         plt.plot()
+
+    def show_semivariograms(self):
+        plt.figure(figsize=(12, 12))
+        plt.plot(self.experimental_point_support[:, 0], self.experimental_point_support[:, 1], color='b')
+        plt.plot(self.experimental_point_support[:, 0], self.base_point_support, color='r',
+                 linestyle='--')
+        plt.plot(self.optimal_regularized_model[:, 0], self.optimal_regularized_model[:, 1], color='g',
+                 linestyle='-.')
+        plt.legend(['Empirical semivariogram', 'Theoretical semivariogram',
+                    'Regularized semivariogram, iteration {}'.format(self.iteration_number)])
+        plt.title('Semivariograms comparison')
+        plt.xlabel('Distance')
+        plt.ylabel('Semivariance')
+        plt.show()

pyinterpolate/kriging/semivariance_areal.py
@@ -12,11 +12,13 @@ from pyinterpolate.kriging.helper_functions.euclidean_distance import calculate_
 
 class ArealSemivariance(Semivariance):
 
-    def __init__(self, areal_data_file, areal_lags, areal_step_size, data_column,
+    def __init__(self, model,
+                 areal_data_file, areal_lags, areal_step_size, data_column,
                  population_data_file, population_value_column, population_lags, population_step_size,
                  id_column_name):
         super().__init__(areal_data_file, areal_lags, areal_step_size, id_column_name)
 
+        self.semivariance_model = model
         self.population = population_data_file
         self.val_col = population_value_column
         self.pop_lags = population_lags
@@ -32,6 +34,20 @@ class ArealSemivariance(Semivariance):
         self.semivariogram_between_blocks = None
         self.block_semivariogram = None
 
+    def calculate_semivariances(self, distances):
+        """Method predicts semivariance at a given distance. Method uses semivariance_model which is
+        initialized with the class itself.
+
+        INPUT:
+        :param distances: list of distances between points.
+
+        OUTPUT:
+        :return predictions: list of semivariances for a given distances list.
+        """
+
+        predictions = self.semivariance_model.predict(distances)
+        return predictions
+
     def blocks_semivariance(self, within_block_semivariogram=None, semivariogram_between_blocks=None):
         """Function calculates regularized point support semivariogram in the form given in:
         Goovaerts P., Kriging and Semivariogram Deconvolution in the Presence of Irregular Geographical Units,
@@ -43,6 +59,12 @@ class ArealSemivariance(Semivariance):
         gamma_h(v, v) - arithmetical average of within-block semivariogram
 
         INPUT:
+        :param within_block_semivariogram: mean semivariance between the blocks:
+        yh(v, v) = 1 / (2*N(h)) SUM(from a=1 to N(h)) [y(va, va) + y(va+h, va+h)], where:
+        y(va, va) and y(va+h, va+h) are the inblock semivariances of block a and block a+h separated
+        by the distance h weighted by the inblock population.
+        :param semivariogram_between_blocks: semivariance between all blocks calculated from the theoretical
+        model.
 
         OUTPUT:
         :return: semivariance: numpy array of pair of lag and semivariance values where
@@ -57,8 +79,7 @@ class ArealSemivariance(Semivariance):
             semivariogram_between_blocks = self.calculate_general_block_to_block_semivariogram()
 
         blocks_semivar = semivariogram_between_blocks
-
-        blocks_semivar[:, 1] = blocks_semivar[:, 1] - within_block_semivariogram[:, 1]
+        blocks_semivar[:, 1] = np.abs(semivariogram_between_blocks[:, 1] - within_block_semivariogram[:, 1])
         self.block_semivariogram = blocks_semivar
 
         return blocks_semivar
@@ -146,7 +167,8 @@ class ArealSemivariance(Semivariance):
             # DATA PROCESSING INTO ARRAY
             block_points = joined_population_points[joined_population_points[self.id_field] == single_id]
 
-            points_array = super()._get_posx_posy(block_points, self.val_col, areal=False, dropna=False, points_type=True)
+            points_array = super()._get_posx_posy(block_points, self.val_col, areal=False, dropna=False,
+                                                  points_type=True)
             block_dict[single_id]['coordinates'] = points_array  # update block_dict
             block_dict[single_id]['rate'] = (areas[self.data_col][areas[self.id_field] == single_id]).values[0]
 
@@ -156,8 +178,11 @@ class ArealSemivariance(Semivariance):
 
             distances = calculate_distance(points_array)
             try:
-                semivariance = super()._calculate_semivars(self.pop_lags, self.pop_step, points_array, distances)
-                semivar = np.sum(semivariance[1, :]) / p_squared
+                semivariance = self.calculate_semivariances(distances)
+                try:
+                    semivar = np.sum(semivariance[1, :]) / p_squared
+                except IndexError:
+                    semivar = np.sum(semivariance[0][0]) / p_squared
             except ValueError:
                 print('Area: {}, Value error, semivariance set to 0'.format(single_id))
                 semivar = 0
@@ -228,8 +253,7 @@ class ArealSemivariance(Semivariance):
 
     # XXXXXXXXXXXXXXXXXXXX BLOCK-BLOCK SEMIVARIOGRAM PART XXXXXXXXXXXXXXXXXXXX
 
-    @staticmethod
-    def _calculate_semivariance_block_pair(block_a_data, block_b_data):
+    def _calculate_semivariance_block_pair(self, block_a_data, block_b_data):
         a_len = len(block_a_data)
         b_len = len(block_b_data)
         pa_pah = a_len * b_len
@@ -237,8 +261,9 @@ class ArealSemivariance(Semivariance):
         for point1 in block_a_data:
             variances = []
             for point2 in block_b_data:
-                smv = point1[-1] - point2[-1]
-                smv = smv ** 2
+                smv = self.semivariance_model.predict(
+                    np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+                )
                 variances.append(smv)
             variance = np.sum(variances) / (2 * len(variances))
             semivariance.append(variance)