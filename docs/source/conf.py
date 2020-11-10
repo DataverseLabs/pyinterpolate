7970debb8bff253996d40f206483e7a0967550ff
szymon-datalions <s.molinski@datalions.eu>
2020-11-10 16:45:51 +0100

    updated documentation - license, contribution, tutorials

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,9 @@ extensions = [
     	'sphinx.ext.intersphinx',
     	'sphinx.ext.ifconfig',
     	'sphinx.ext.viewcode',
-    	'sphinx.ext.githubpages'
+    	'sphinx.ext.githubpages',
+        'nbsphinx',
+        'sphinx.ext.mathjax',
 	]
 
 # Add any paths that contain templates here, relative to this directory.
