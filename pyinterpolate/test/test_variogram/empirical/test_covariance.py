3fe253d15f7959b5baa5243b8ec4a5c2e3446fd0
Szymon Moliński <simon@ml-gis-service.com>
2022-01-24 22:44:34 +0200

    First iter of EmpiricalVariogram class

--- pyinterpolate/test/test_variogram/empirical/test_covariance.py
+++ pyinterpolate/test/test_variogram/empirical/test_covariance.py
@@ -35,12 +35,11 @@ REFERENCE_INPUT_ZEROS = np.array([
 # EXPECTED OUTPUTS
 
 EXPECTED_OUTPUT_OMNI = np.array([
-    [0, 4.248, 0, 0],
-    [1, -0.543, 24, 30.71],
-    [2, -0.795, 22, 30.25],
-    [3, -1.26, 20, 31.36],
-    [4, -0.197, 18, 30.864],
-    [5, 1.234, 16, 28.891]
+    [1, -0.543, 24],
+    [2, -0.795, 22],
+    [3, -1.26, 20],
+    [4, -0.197, 18],
+    [5, 1.234, 16]
 ])
 
 EXPECTED_VARIANCE_CO_OUTPUT = 4.248
@@ -55,7 +54,6 @@ EXPECTED_OUTPUT_ARMSTRONG_LAG1 = 6.649
 # CONSTS
 STEP_SIZE = 1
 MAX_RANGE = 6
-RETURN_MEAN_SQUARED = True
 
 
 class TestCovariance(unittest.TestCase):
@@ -63,23 +61,22 @@ class TestCovariance(unittest.TestCase):
     # OMNIDIRECTIONAL CASES
 
     def test_variance_c0(self):
-        output = calculate_covariance(REFERENCE_INPUT_WE, step_size=STEP_SIZE, max_range=MAX_RANGE,
-                                      return_lag_squared_means=RETURN_MEAN_SQUARED)
-        c0 = output[0][1]
+        output = calculate_covariance(REFERENCE_INPUT_WE, step_size=STEP_SIZE, max_range=MAX_RANGE)
+        c0 = output[1]
         msg = f'Expected variance of the input dataset is {EXPECTED_VARIANCE_CO_OUTPUT} but ' \
               f'{c0} was returned.'
         self.assertAlmostEqual(c0, EXPECTED_VARIANCE_CO_OUTPUT, 2, msg=msg)
 
     def test_calculate_covariance_single_row(self):
-        output = calculate_covariance(REFERENCE_INPUT_WE, step_size=STEP_SIZE, max_range=MAX_RANGE,
-                                      return_lag_squared_means=RETURN_MEAN_SQUARED)
+        output = calculate_covariance(REFERENCE_INPUT_WE, step_size=STEP_SIZE, max_range=MAX_RANGE)
+        output = output[0]
         are_close = np.allclose(output, EXPECTED_OUTPUT_OMNI, rtol=1.e-2)
         msg = 'The difference between expected values and calculated covariances are too large, check calculations.'
         self.assertTrue(are_close, msg)
 
     def test_calculate_covariance_zeros_omni(self):
         output = calculate_covariance(REFERENCE_INPUT_ZEROS, step_size=STEP_SIZE, max_range=MAX_RANGE)
-        mean_val = np.mean(output[:, 1])
+        mean_val = np.mean(output[0][:, 1])
         msg = 'Calculated covariance should be equal to zero if we provide only zeros array.'
         self.assertEqual(mean_val, EXPECTED_OUTPUT_ZEROS, msg)
 
@@ -90,7 +87,7 @@ class TestCovariance(unittest.TestCase):
         path_to_the_data = os.path.join(my_dir, filepath)
         arr = np.load(path_to_the_data)
         cov = calculate_covariance(arr, 1, 2, get_c0=False)
-        lag1_test_value = cov[0][1]
+        lag1_test_value = cov[0][0][1]
         err_msg = f'Calculated covariance for lag 1 should be equal to {EXPECTED_OUTPUT_ARMSTRONG_LAG1} for ' \
                   f'omnidirectional case and points within {filename} file.'
         self.assertAlmostEqual(lag1_test_value, EXPECTED_OUTPUT_ARMSTRONG_LAG1, places=2, msg=err_msg)
@@ -104,7 +101,7 @@ class TestCovariance(unittest.TestCase):
         path_to_the_data = os.path.join(my_dir, filepath)
         arr = np.load(path_to_the_data)
         cov = calculate_covariance(arr, 1, 2, direction=0, tolerance=0.01, get_c0=False)
-        lag1_test_value = cov[0][1]
+        lag1_test_value = cov[0][0][1]
         err_msg = f'Calculated covariance for lag 1 should be equal to {EXPECTED_OUTPUT_ARMSTRONG_NS_LAG1} for ' \
                   f'N-S direction case and points within {filename} file.'
         self.assertAlmostEqual(lag1_test_value, EXPECTED_OUTPUT_ARMSTRONG_NS_LAG1, places=2, msg=err_msg)
@@ -116,6 +113,7 @@ class TestCovariance(unittest.TestCase):
         path_to_the_data = os.path.join(my_dir, filepath)
         arr = np.load(path_to_the_data)
         cov = calculate_covariance(arr, 1, 2, direction=90, tolerance=0.1, get_c0=False)
+        cov = cov[0]
         lag1_test_value = cov[0][1]
         err_msg = f'Calculated semivariance for lag 1 should be equal to {EXPECTED_OUTPUT_ARMSTRONG_WE_LAG1} for ' \
                   f'W-E direction case and points within {filename} file.'
@@ -128,6 +126,7 @@ class TestCovariance(unittest.TestCase):
         path_to_the_data = os.path.join(my_dir, filepath)
         arr = np.load(path_to_the_data)
         cov = calculate_covariance(arr, 1, 4, direction=135, tolerance=0.01, get_c0=False)
+        cov = cov[0]
         lag1_test_value = cov[1][1]
         err_msg = f'Calculated covariance for lag 1 should be equal to {EXPECTED_OUTPUT_ARMSTRONG_NW_SE_LAG2} for ' \
                   f'NW-SE direction case and points within {filename} file.'
@@ -140,6 +139,7 @@ class TestCovariance(unittest.TestCase):
         path_to_the_data = os.path.join(my_dir, filepath)
         arr = np.load(path_to_the_data)
         cov = calculate_covariance(arr, 1, 3, direction=45, tolerance=0.01, get_c0=False)
+        cov = cov[0]
         lag1_test_value = cov[1][1]
         err_msg = f'Calculated coivariance for lag 1 should be equal to {EXPECTED_OUTPUT_ARMSTRONG_NE_SW_LAG2} for ' \
                   f'NE-SW direction case and points within {filename} file.'
--- pyinterpolate/variogram/empirical/empirical_semivariogram.py
+++ pyinterpolate/variogram/empirical/empirical_semivariogram.py
@@ -1,2 +1,49 @@
+from pyinterpolate.variogram.empirical.covariance import calculate_covariance
+from pyinterpolate.variogram.empirical.semivariance import calculate_semivariance
+
+
 class EmpiricalSemivariogram:
-    pass
+
+    def __init__(self, input_array, step_size: float, max_range: float, weights=None, direction=0, tolerance=0,
+                 calculate_semivariance=True, calculate_covariance=False, get_variance_c0=False):
+
+        self.input_array = input_array
+        self.experimental_semivariance = None
+        self.experimental_covariance = None
+        self.variance = None
+
+        self.step = step_size
+        self.mx_rng = max_range
+        self.weights = weights
+        self.direct = direction
+        self.tol = tolerance
+
+        if calculate_semivariance:
+            self._calculate_semivariance()
+        if calculate_covariance:
+            self._calculate_covariance(get_variance_c0)
+
+
+        self.is_semivariance = calculate_semivariance
+        self.is_covariance = calculate_covariance
+        self.is_variance = get_variance_c0
+
+    def _calculate_semivariance(self):
+        self.experimental_semivariance = calculate_semivariance(
+            points=self.input_array.copy(),
+            step_size=self.step,
+            max_range=self.mx_rng,
+            weights=self.weights,
+            direction=self.direct,
+            tolerance=self.tol
+        )
+
+    def _calculate_covariance(self, get_variance=False):
+        self.experimental_covariance, self.variance = calculate_covariance(
+            points=self.input_array.copy(),
+            step_size=self.step,
+            max_range=self.mx_rng,
+            direction=self.direct,
+            tolerance=self.tol,
+            get_c0=get_variance
+        )
