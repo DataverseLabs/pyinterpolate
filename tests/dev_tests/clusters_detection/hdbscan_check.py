acca67a3eb0fbb5a6d9e8f32947e213f83455233
Simon <31246246+SimonMolinsky@users.noreply.github.com>
2023-02-22 23:06:37 +0100

    hdbscan tests update

---
+++ tests/dev_tests/clusters_detection/hdbscan_check.py
@@ -0,0 +1,52 @@
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
+
+from pyinterpolate import Blocks
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+import matplotlib as mpl
+
+import hdbscan
+mpl.use('TkAgg')  # !IMPORTANT
+
+
+DATASET = '../../samples/regularization/cancer_data.gpkg'
+POLYGON_LAYER = 'areas'
+POLYGON_ID = 'FIPS'
+POLYGON_VALUE = 'rate'
+NN = 8
+
+AREAL_INPUT = Blocks()
+AREAL_INPUT.from_file(DATASET, value_col=POLYGON_VALUE, index_col=POLYGON_ID, layer_name=POLYGON_LAYER)
+
+centroids = AREAL_INPUT.data[[AREAL_INPUT.cx, AREAL_INPUT.cy]].values
+
+# plot
+xss = centroids[:, 0]
+yss = centroids[:, 1]
+
+
+# Detect clusters and show those
+clusterer = hdbscan.HDBSCAN(
+
+)
+
+clusterer.fit(centroids)
+
+lbls = clusterer.labels_
+no_of_labels = len(np.unique(lbls))
+
+color = cm.get_cmap('Pastel1')
+clist = [color(i) for i in range(color.N)]
+cmap = LinearSegmentedColormap.from_list('Custom cmap', clist, color.N)
+bounds = np.linspace(0, no_of_labels, no_of_labels+1)
+norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+fig, ax = plt.subplots(1,1)
+scat = ax.scatter(xss, yss, c=lbls, cmap=cmap, norm=norm)
+cb = plt.colorbar(scat, spacing='proportional', ticks=bounds)
+cb.set_label('Labels')
+plt.show()
+
+
+
