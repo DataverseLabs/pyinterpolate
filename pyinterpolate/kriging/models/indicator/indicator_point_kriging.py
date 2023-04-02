3aa25a09df7faa2b8f30a730aca3ec7d8109196a
Simon <31246246+SimonMolinsky@users.noreply.github.com>
2023-04-02 09:35:37 +0200

    indicator kriging alpha

--- pyinterpolate/kriging/models/indicator/indicator_point_kriging.py
+++ pyinterpolate/kriging/models/indicator/indicator_point_kriging.py
@@ -17,9 +17,12 @@ Indicator kriging is performed for each threshold using four types of destinatio
 3) sampled locations (cross-validation option), and
 4) set of test locations (jack-knife option).
 """
-from typing import Dict
+from typing import Tuple
 
 import numpy as np
+from tqdm import tqdm
+
+from scipy.interpolate import UnivariateSpline
 
 from pyinterpolate.kriging.models.point.ordinary_kriging import ordinary_kriging
 from pyinterpolate.kriging.models.point.simple_kriging import simple_kriging
@@ -36,7 +39,8 @@ class IndicatorKriging:
                  process_mean: float = None,
                  neighbors_range=None,
                  no_neighbors=4,
-                 use_all_neighbors_in_range=False):
+                 use_all_neighbors_in_range=False,
+                 allow_approximate_solutions=False):
         """
         Class performs indicator kriging.
 
@@ -69,50 +73,117 @@ class IndicatorKriging:
         use_all_neighbors_in_range : bool, default = False
             ``True``: if the real number of neighbors within the ``neighbors_range`` is greater than the
             ``number_of_neighbors`` parameter then take all of them anyway.
+
+        allow_approximate_solutions : bool, default=False
+            Allows the approximation of kriging weights based on the OLS algorithm. We don't recommend set it to
+            ``True`` if you don't know what are you doing. This parameter can be useful when you have clusters in
+            your dataset, that can lead to singular or near-singular matrix creation.
         """
 
-        self.indicator_outputs = self._estimate(datapoints,
-                                                indicator_variograms,
-                                                unknown_locations,
-                                                kriging_type,
-                                                process_mean,
-                                                neighbors_range,
-                                                no_neighbors,
-                                                use_all_neighbors_in_range)
-        
-        self.raw_probabilities = self._get_raw_probabilities()
+        self.thresholds = np.array(list(indicator_variograms.theoretical_indicator_variograms.keys())).astype(float)
+        self.coordinates = unknown_locations.copy()
 
+        self.indicator_predictions = self._estimate(datapoints,
+                                                    indicator_variograms,
+                                                    unknown_locations,
+                                                    kriging_type,
+                                                    process_mean,
+                                                    neighbors_range,
+                                                    no_neighbors,
+                                                    use_all_neighbors_in_range,
+                                                    allow_approximate_solutions)
 
-    def _estimate(self, known_points, variograms, unknowns, ktype, pmean, nrange, no_neigh, all_in_rng) -> Dict:
+        self.expected_values, self.variances = self.get_expected_values()
+
+    def _estimate(self,
+                  datapoints: np.ndarray,
+                  indicator_variograms: IndicatorVariograms,
+                  unknown_locations: np.ndarray,
+                  kriging_type: str = 'ok',
+                  process_mean: float = None,
+                  neighbors_range=None,
+                  no_neighbors=4,
+                  use_all_neighbors_in_range=False,
+                  allow_approximate_solutions=False) -> np.ndarray:
         """
-        Function performs indicator kriging for each threshold.
-        """
-        indicator_outputs = {}
+        Method estimates probabilities of a location becoming within a given threshold.
 
-        for _key, _item in variograms.theoretical_indicator_variograms.items():
+        Parameters
+        ----------
+        datapoints : numpy ndarray
+            The known locations ``[x, y, value]``.
+
+        indicator_variograms : IndicatorVariograms
+            Modeled variograms for each threshold.
+
+        unknown_locations : numpy ndarray
+            Points where we want to estimate value ``(x, y) <-or-> (lon, lat)``.
+
+        kriging_type : str, default = 'ok'
+            Type of kriging to perform. Possible values: 'ok' - ordinary kriging, 'sk' - simple kriging.
+
+        process_mean : float
+            The mean value of a process over a study area. Should be know before processing. That's why Simple
+            Kriging has a limited number of applications. You must have multiple samples and well-known area to
+            know this parameter.
+
+        neighbors_range : float, default=None
+            The maximum distance where we search for neighbors. If ``None`` is given then range is selected from
+            the ``theoretical_model`` ``rang`` attribute.
+
+        no_neighbors : int, default = 4
+            The number of the **n-closest neighbors** used for interpolation.
+
+        use_all_neighbors_in_range : bool, default = False
+            ``True``: if the real number of neighbors within the ``neighbors_range`` is greater than the
+            ``number_of_neighbors`` parameter then take all of them anyway.
+
+        allow_approximate_solutions : bool, default=False
+            Allows the approximation of kriging weights based on the OLS algorithm. We don't recommend set it to
+            ``True`` if you don't know what are you doing. This parameter can be useful when you have clusters in
+            your dataset, that can lead to singular or near-singular matrix creation.
+
+        Returns
+        -------
+        indicator_predictions : numpy array
+            The indicator probabilities where each column is a threshold.
+
+        Raises
+        ------
+        ValueError
+            Wrong Kriging type, allowed types: 'ok' or 'sk'
+        """
+        predictions_all = []
+
+        for _key, _item in tqdm(indicator_variograms.theoretical_indicator_variograms.items()):
+
+            indicator_points = datapoints.copy()
+            indicator_points[:, -1] = (indicator_points[:, -1] <= float(_key)).astype(int)
+
             predictions = []
-            errors = []
 
-            for _point in unknowns:
-                if ktype == 'ok':
+            for _point in unknown_locations:
+                if kriging_type == 'ok':
                     _pred_arr = ordinary_kriging(
                         theoretical_model=_item,
-                        known_locations=known_points,
+                        known_locations=indicator_points,
                         unknown_location=_point,
-                        neighbors_range=nrange,
-                        no_neighbors=no_neigh,
-                        use_all_neighbors_in_range=all_in_rng
+                        neighbors_range=neighbors_range,
+                        no_neighbors=no_neighbors,
+                        use_all_neighbors_in_range=use_all_neighbors_in_range,
+                        allow_approximate_solutions=allow_approximate_solutions
                     )
 
-                elif ktype == 'sk':
+                elif kriging_type == 'sk':
                     _pred_arr = simple_kriging(
                         theoretical_model=_item,
-                        known_locations=known_points,
+                        known_locations=indicator_points,
                         unknown_location=_point,
-                        process_mean=pmean,
-                        neighbors_range=nrange,
-                        no_neighbors=no_neigh,
-                        use_all_neighbors_in_range=all_in_rng
+                        process_mean=process_mean,
+                        neighbors_range=neighbors_range,
+                        no_neighbors=no_neighbors,
+                        use_all_neighbors_in_range=use_all_neighbors_in_range,
+                        allow_approximate_solutions=allow_approximate_solutions
                     )
 
                 else:
@@ -120,11 +191,70 @@ class IndicatorKriging:
                                      '"ok" - ordinary kriging, "sk" - simple kriging.')
 
                 predictions.append(_pred_arr[0])
-                errors.append(_pred_arr[1])
 
-            indicator_outputs[_key] = {
-                'predictions': np.array(predictions),
-                'errors': np.array(errors)
-            }
+            predictions_all.append(predictions)
 
-        return indicator_outputs
+        indicator_predictions = np.column_stack(predictions_all)
+
+        indicator_predictions = self._clean_probabilities(indicator_predictions)
+
+        return indicator_predictions
+
+    def get_expected_values(self, ccdf_density=100) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Function gets expected values and their variance for each point based on the ccdf function.
+
+        Returns
+        -------
+
+        """
+
+        expected_values = []
+        expected_variances = []
+
+        for row in self.indicator_predictions:
+            old_indices = self.thresholds.copy()
+            new_length = 100
+            new_indices = np.linspace(old_indices.min(), old_indices.max(), new_length)
+            spl = UnivariateSpline(old_indices, row, s=0)
+            new_tvals = spl(new_indices)
+
+            cdf = new_tvals.cumsum() / new_tvals.sum()
+            ccdf = 1 - cdf
+            expected_c = np.mean(ccdf)
+            expected_c_index = np.searchsorted(np.flip(ccdf), expected_c)
+            expected_c_index = len(ccdf) - expected_c_index
+            expected_value = new_indices[expected_c_index]
+
+            expected_values.append(expected_value)
+
+            # Variance
+            expected_variance = np.mean(
+                np.abs(new_tvals - expected_value) ** 2
+            )
+            expected_variances.append(expected_variance)
+
+        return np.array(expected_values), np.array(expected_variances)
+
+    def _clean_probabilities(self, array: np.ndarray):
+        """
+        Function normalizes and transform given probabilities.
+
+        Parameters
+        ----------
+        array : numpy array
+            Array which rows are transformed.
+
+        Returns
+        -------
+        transformed : numpy array
+            Transformed (normalized along rows) array.
+        """
+        arr = self.__cut_to_zero_one(array).copy()
+        return arr
+
+    @staticmethod
+    def __cut_to_zero_one(arr):
+        arr[arr > 1] = 1
+        arr[arr < 0] = 0
+        return arr
