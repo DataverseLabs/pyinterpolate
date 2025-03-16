5e99c75058718cf38003ed4cf9ffacb68fe18c45
Simon <31246246+SimonMolinsky@users.noreply.github.com>
2025-03-16 17:50:43 +0200

    Update ptp_warnings.py

--- src/pyinterpolate/core/ptp_warnings/ptp_warnings.py
+++ src/pyinterpolate/core/ptp_warnings/ptp_warnings.py
@@ -3,14 +3,18 @@ Authors
 -------
 1. Szymon Moliński | @SimonMolinsky
 """
+
+
 class ZerosMatrixWarning(Warning):
     """
     Warning invoked if any matrix in kriging system is populated with zeros.
     """
 
     def __init__(self):
-        self.message = 'Matrix in your Kriging system is populated only be zeros. It is probably data error.' \
-                       ' Prediction will return 0 as a predicted value and np.nan as an error.'
+        self.message = ('Matrix in your Kriging system is populated only '
+                        'be zeros. It is probably data error. '
+                        'Prediction will return 0 as a predicted value '
+                        'and np.nan as an error.')
 
     def __str__(self):
         return repr(self.message)
@@ -18,11 +22,13 @@ class ZerosMatrixWarning(Warning):
 
 class LeastSquaresApproximationWarning(Warning):
     """
-    Warning used when algorithm searches for approximate solution instead of exact solution.
+    Warning used when algorithm searches for approximate solution instead
+    of the exact solution.
     """
 
     def __init__(self):
-        self.message = 'Kriging system solution is based on the approximate solution, output may be incorrect!'
+        self.message = ('Kriging system solution is based on the '
+                        'approximate solution, output may be incorrect!')
 
     def __str__(self):
         return repr(self.message)
