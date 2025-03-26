73c053ea06f371d568b6ac17c2e6a785e7f37246
Simon <31246246+SimonMolinsky@users.noreply.github.com>
2025-03-26 19:26:57 +0200

    Update simple.py

--- src/pyinterpolate/kriging/point/simple.py
+++ src/pyinterpolate/kriging/point/simple.py
@@ -10,7 +10,9 @@ from typing import List, Union, Tuple
 
 import numpy as np
 
-from pyinterpolate.kriging.utils.point_kriging_solve import get_predictions, solve_weights
+from kriging.utils.errors import singular_matrix_error
+from pyinterpolate.kriging.utils.point_kriging_solve import (get_predictions,
+                                                             solve_weights)
 from pyinterpolate.semivariogram.theoretical.classes.theoretical_variogram import TheoreticalVariogram
 
 
@@ -26,42 +28,46 @@ def simple_kriging(
         allow_approximate_solutions=False
 ) -> List:
     """
-    Function predicts value at unknown location with Ordinary Kriging technique.
+    Function predicts value at unknown location using Simple Kriging.
 
     Parameters
     ----------
     theoretical_model : TheoreticalVariogram
-        A trained theoretical variogram model.
+        Fitted theoretical variogram model.
 
     known_locations : numpy array
-        The known locations.
+        The known locations: ``[x, y, value]``.
 
     unknown_location : Union[List, Tuple, numpy array]
         Point where you want to estimate value ``(x, y) <-> (lon, lat)``.
 
     process_mean : float
-        The mean value of a process over a study area. Should be know before processing. That's why Simple
-        Kriging has a limited number of applications. You must have multiple samples and well-known area to
-        know this parameter.
+        The mean value of a process over a study area. Should be known
+        before processing. That's why Simple Kriging has a limited number
+        of applications. You must have multiple samples and well-known area
+        to know this parameter.
 
     neighbors_range : float, default=None
-        The maximum distance where we search for neighbors. If ``None`` is given then range is selected from
-        the ``theoretical_model`` ``rang`` attribute.
+        The maximum distance where we search for neighbors.  If ``None`` is
+        given then range is selected from the Theoretical Model.
 
     no_neighbors : int, default = 4
         The number of the **n-closest neighbors** used for interpolation.
 
     max_tick : float, default=5.
-        If searching for neighbors in a specific direction how big should be a tolerance for increasing
-        the search angle (how many degrees more).
+        If searching for neighbors in a specific direction how large should
+        be tolerance for increasing the search angle (how many degrees more).
 
     use_all_neighbors_in_range : bool, default = False
-        ``True``: if the real number of neighbors within the ``neighbors_range`` is greater than the
+        ``True``: if the real number of neighbors within
+        the ``neighbors_range`` is greater than the
         ``number_of_neighbors`` parameter then take all of them anyway.
 
     allow_approximate_solutions : bool, default=False
-        Allows the approximation of kriging weights based on the OLS algorithm. We don't recommend set it to ``True``
-        if you don't know what are you doing. This parameter can be useful when you have clusters in your dataset,
+        Allows the approximation of kriging weights based on the OLS
+        algorithm. We don't recommend set it to ``True``
+        if you don't know what are you doing. This parameter can be useful
+        when you have clusters in your dataset,
         that can lead to singular or near-singular matrix creation.
 
     Returns
@@ -72,7 +78,7 @@ def simple_kriging(
     Raises
     ------
     RunetimeError
-        Singularity matrix in a Kriging system.
+        Singular matrix in Kriging system.
     """
     k, predicted, dataset = get_predictions(theoretical_model,
                                             known_locations,
@@ -83,19 +89,19 @@ def simple_kriging(
                                             max_tick)
 
     try:
-        output_weights = solve_weights(predicted, k, allow_approximate_solutions)
+        output_weights = solve_weights(predicted,
+                                       k,
+                                       allow_approximate_solutions)
+        r = dataset[:, -2] - process_mean
+        zhat = r.dot(output_weights)
+        zhat = zhat + process_mean
+
+        sigma = np.matmul(output_weights.T, k)
+
+        if sigma < 0:
+            return [zhat, np.nan, unknown_location[0], unknown_location[1]]
+
+        return [zhat, sigma, unknown_location[0], unknown_location[1]]
+
     except np.linalg.LinAlgError as _:
-        msg = 'Singular matrix in Kriging system detected, check if you have duplicated coordinates ' \
-              'in the ``known_locations`` variable.'
-        raise RuntimeError(msg)
-
-    r = dataset[:, -2] - process_mean
-    zhat = r.dot(output_weights)
-    zhat = zhat + process_mean
-
-    sigma = np.matmul(output_weights.T, k)
-
-    if sigma < 0:
-        return [zhat, np.nan, unknown_location[0], unknown_location[1]]
-
-    return [zhat, sigma, unknown_location[0], unknown_location[1]]
+        singular_matrix_error()
