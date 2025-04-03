3822ed3f61baaa7fa28dc0238e3767098cbf127d
Simon <31246246+SimonMolinsky@users.noreply.github.com>
2025-04-03 10:09:32 +0300

    Update point_to_block_semivariance.py

--- src/pyinterpolate/semivariogram/deconvolution/point_to_block_semivariance.py
+++ src/pyinterpolate/semivariogram/deconvolution/point_to_block_semivariance.py
@@ -5,16 +5,19 @@ import pandas as pd
 from pyinterpolate.semivariogram.theoretical.classes.theoretical_variogram import TheoreticalVariogram
 
 
-def calculate_average_p2b_semivariance(ds: pd.DataFrame,
-                                       semivariogram_model: TheoreticalVariogram,
-                                       block_x_coo_col: Union[Hashable, str],
-                                       block_y_coo_col: Union[Hashable, str],
-                                       block_val_col: Union[Hashable, str],
-                                       neighbor_idx_col: Union[Hashable, str],
-                                       neighbor_val_col: Union[Hashable, str],
-                                       distance_col: Union[Hashable, str], ) -> pd.Series:
+def calculate_average_p2b_semivariance(
+        ds: pd.DataFrame,
+        semivariogram_model: TheoreticalVariogram,
+        block_x_coo_col: Union[Hashable, str],
+        block_y_coo_col: Union[Hashable, str],
+        block_val_col: Union[Hashable, str],
+        neighbor_idx_col: Union[Hashable, str],
+        neighbor_val_col: Union[Hashable, str],
+        distance_col: Union[Hashable, str],
+) -> pd.Series:
     """
-    Function calculates average semivariance a single point from block Pj and all points from block Pi.
+    Function calculates average semivariance between a single point from
+    the block Pj and all other points from the block Pi.
 
     Parameters
     ----------
@@ -26,6 +29,27 @@ def calculate_average_p2b_semivariance(ds: pd.DataFrame,
           * neighbor index
           * neighboring block point support value
           * distance to the point in neighboring block
+
+    semivariogram_model : TheoreticalVariogram
+        Fitted variogram model.
+
+    block_x_coo_col : Union[Hashable, str]
+        Column name with block point support coordinate x.
+
+    block_y_coo_col: Union[Hashable, str]
+        Column name with block point support coordinate y.
+
+    block_val_col: Union[Hashable, str]
+        Column name with block point support value.
+
+    neighbor_idx_col: Union[Hashable, str]
+        Column name with neighbor index.
+
+    neighbor_val_col: Union[Hashable, str]
+        Column name with neighbor's point support value.
+
+    distance_col: Union[Hashable, str]
+        Column with distance to the point in neighboring block.
 
     Returns
     -------
@@ -43,7 +67,9 @@ def calculate_average_p2b_semivariance(ds: pd.DataFrame,
     ds['gamma_weights'] = ds['gamma'] * ds['custom_weights']
 
     # groupby
-    gs = ds.groupby([block_x_coo_col, block_y_coo_col, neighbor_idx_col])[['custom_weights', 'gamma_weights']].sum()
+    gs = ds.groupby(
+        [block_x_coo_col, block_y_coo_col, neighbor_idx_col]
+    )[['custom_weights', 'gamma_weights']].sum()
 
     # Calculate weighted semivariance
     weighted_semivariance = gs['gamma_weights'] / gs['custom_weights']
