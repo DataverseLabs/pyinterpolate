6a3ef106597071a037660dc229e170321c123867
Simon <31246246+SimonMolinsky@users.noreply.github.com>
2025-04-05 11:21:32 +0300

    Update select_points.py

--- src/pyinterpolate/transform/select_points.py
+++ src/pyinterpolate/transform/select_points.py
@@ -3,7 +3,8 @@ from typing import Iterable
 import numpy as np
 from numpy.typing import ArrayLike
 
-from pyinterpolate.distance.angular import calc_angles, calculate_angular_difference
+from pyinterpolate.distance.angular import calc_angles, \
+    calculate_angular_difference
 from pyinterpolate.distance.point import point_distance
 
 
@@ -11,10 +12,12 @@ def create_min_max_array(value: float,
                          min_scaling_factor: float,
                          max_scaling_factor: float,
                          number_of_steps: int) -> np.ndarray:
-    """Function prepares a numpy array of N equidistant values between (a:b), where:
-    * N - number of steps,
-    * a - min_scaling_factor * value,
-    * b - max_scaling_factor * value.
+    """
+    Function prepares a numpy array of N equidistant values between (a:b),
+    where:
+      * N - number of steps,
+      * a - min_scaling_factor * value,
+      * b - max_scaling_factor * value.
 
     Parameters
     ----------
@@ -36,13 +39,16 @@ def create_min_max_array(value: float,
     return min_max_steps
 
 
-def select_kriging_data(unknown_position: ArrayLike,
-                        data_array: np.ndarray,
-                        neighbors_range: float,
-                        number_of_neighbors: int = 4,
-                        use_all_neighbors_in_range: bool = False) -> np.ndarray:
+def select_kriging_data(
+        unknown_position: ArrayLike,
+        data_array: np.ndarray,
+        neighbors_range: float,
+        number_of_neighbors: int = 4,
+        use_all_neighbors_in_range: bool = False
+) -> np.ndarray:
     """
-    Function prepares data for kriging - array of point position, value and distance to an unknown point.
+    Function prepares data for kriging - array of point position,
+    value and distance to an unknown point.
 
     Parameters
     ----------
@@ -53,20 +59,23 @@ def select_kriging_data(unknown_position: ArrayLike,
         Known points.
 
     neighbors_range : float
-        Range within neighbors are affecting the value, it should be close or the same as the variogram range.
+        Range within neighbors are affecting the value, it should be
+        close or the same as the variogram range.
 
     number_of_neighbors : int, default = 4
         Number of the n-closest neighbors used for interpolation.
 
     use_all_neighbors_in_range : bool, default = False
-        ``True``: if number of neighbors within the ``neighbors_range`` is greater than
-        the ``number_of_neighbors`` then use all neighbors in range.
+        ``True``: if number of neighbors within the ``neighbors_range``
+        is greater than the ``number_of_neighbors`` then use all
+        neighbors in the range.
 
     Returns
     -------
     : numpy array
-        Dataset of the length ``number_of_neighbors`` <= length. Each record is created from the position, value and
-        distance to the unknown point ``[[x, y, value, distance to unknown position]]``.
+        Dataset of the length ``number_of_neighbors`` <= length. Each record
+        is created from the position, value and distance to the unknown
+        point ``[[x, y, value, distance to unknown position]]``.
 
     """
 
@@ -78,8 +87,10 @@ def select_kriging_data(unknown_position: ArrayLike,
 
     # Prepare data for kriging
     neighbors_and_dists = np.c_[data_array, dists.T]
-    sorted_neighbors_and_dists = neighbors_and_dists[neighbors_and_dists[:, -1].argsort()]
-    prepared_data = sorted_neighbors_and_dists[sorted_neighbors_and_dists[:, -1] <= neighbors_range, :]
+    sorted_neighbors_and_dists = neighbors_and_dists[
+        neighbors_and_dists[:, -1].argsort()]
+    prepared_data = sorted_neighbors_and_dists[
+                    sorted_neighbors_and_dists[:, -1] <= neighbors_range, :]
 
     if len(prepared_data) >= number_of_neighbors:
         if use_all_neighbors_in_range:
@@ -90,15 +101,18 @@ def select_kriging_data(unknown_position: ArrayLike,
         return sorted_neighbors_and_dists[:number_of_neighbors]
 
 
-def select_kriging_data_from_direction(unknown_position: Iterable,
-                                       data_array: np.ndarray,
-                                       neighbors_range: float,
-                                       direction: float,
-                                       number_of_neighbors: int = 4,
-                                       use_all_neighbors_in_range: bool = False,
-                                       max_tick: int = 10) -> np.ndarray:
+def select_kriging_data_from_direction(
+        unknown_position: Iterable,
+        data_array: np.ndarray,
+        neighbors_range: float,
+        direction: float,
+        number_of_neighbors: int = 4,
+        use_all_neighbors_in_range: bool = False,
+        max_tick: int = 10
+) -> np.ndarray:
     """
-    Function selects closest neighbors based on the specific direction and tolerance.
+    Function selects closest neighbors based on the specific
+    direction and tolerance.
 
     Parameters
     ----------
@@ -109,7 +123,8 @@ def select_kriging_data_from_direction(unknown_position: Iterable,
         Known points.
 
     neighbors_range : float
-        Range within neighbors are affecting the value, it should be lower or the same as the variogram range.
+        Range within neighbors are affecting the value, it should be
+        lower or the same as the variogram range.
 
     direction : float
         The direction of a directional variogram.
@@ -118,7 +133,8 @@ def select_kriging_data_from_direction(unknown_position: Iterable,
         Number of the n-closest neighbors used for interpolation.
 
     use_all_neighbors_in_range : bool, default = False
-        ``True``: if number of neighbors within the ``neighbors_range`` is greater than the ``number_of_neighbors`` then
+        ``True``: if number of neighbors within the ``neighbors_range``
+        is greater than the ``number_of_neighbors`` then
         take all of them for modeling.
 
     max_tick : int, default = 10
@@ -127,8 +143,9 @@ def select_kriging_data_from_direction(unknown_position: Iterable,
     Returns
     -------
     : numpy array
-        Dataset of the length number_of_neighbors <= length. Each record is created from the position, value and
-        distance to the unknown point ``[[x, y, value, distance to unknown position]]``.
+        Dataset of the length number_of_neighbors <= length. Each record is
+        created from the position, value and distance to the unknown
+        point ``[[x, y, value, distance to unknown position]]``.
     """
     # Distances to unknown point
     r = np.array([unknown_position])
@@ -138,13 +155,15 @@ def select_kriging_data_from_direction(unknown_position: Iterable,
     angles = calc_angles(known_pos, origin=unknown_position)
     angle_diffs = calculate_angular_difference(angles, direction)
 
-    selected_data = select_possible_neighbors_angular(data_array,
-                                                      dists,
-                                                      angle_diffs,
-                                                      neighbors_range,
-                                                      number_of_neighbors,
-                                                      use_all_neighbors_in_range,
-                                                      max_tick)
+    selected_data = select_possible_neighbors_angular(
+        data_array,
+        dists,
+        angle_diffs,
+        neighbors_range,
+        number_of_neighbors,
+        use_all_neighbors_in_range,
+        max_tick
+    )
 
     return selected_data[:, :-1]
 
@@ -168,17 +187,20 @@ def select_possible_neighbors_angular(possible_neighbors: np.ndarray,
         The array with distances to each neighbor.
 
     angle_differences : numpy array
-        The array with the minimal direction between expected distance and other points.
+        The array with the minimal direction between expected distance and
+        other points.
 
     max_range : float
-        Range within neighbors are affecting the value, it should be lower or the same as the variogram range.
+        Range within neighbors are affecting the value, it should be lower
+        or the same as the variogram range.
 
     min_number_of_neighbors : int
         Number of the n-closest neighbors used for interpolation.
 
     use_all_neighbors_in_range : bool
-        ``True``: if number of neighbors within the ``neighbors_range`` is greater than the ``number_of_neighbors``
-        then take all of them for modeling.
+        ``True``: if number of neighbors within the ``neighbors_range``
+        is greater than the ``number_of_neighbors`` then take all of
+        them for modeling.
 
     max_tick : int
         How many degrees can be added to the search sphere.
@@ -212,18 +234,14 @@ def select_possible_neighbors_angular(possible_neighbors: np.ndarray,
 
     prepared_data = np.c_[possible_neighbors, distances.T, angle_differences.T]
 
-    # neighbors_dists_and_angles = np.c_[possible_neighbors, distances.T, angle_differences.T]
-
-    # First select only neighbors in a max range
-    # sorted_neighbors_and_dists = neighbors_dists_and_angles[neighbors_dists_and_angles[:, -2].argsort()]
-    # prepared_data = sorted_neighbors_and_dists[sorted_neighbors_and_dists[:, -2] <= max_range, :]
-
     # Limit to a specific direction
-    prepared_data_with_angles = prepared_data[angle_differences <= angular_tolerance]
+    prepared_data_with_angles = prepared_data[
+        angle_differences <= angular_tolerance]
 
     while len(prepared_data_with_angles) < min_number_of_neighbors:
         angular_tolerance = angular_tolerance + 1
-        prepared_data_with_angles = prepared_data[angle_differences <= angular_tolerance]
+        prepared_data_with_angles = prepared_data[
+            angle_differences <= angular_tolerance]
         if angular_tolerance > max_tick:
             break
 
@@ -240,20 +258,21 @@ def select_possible_neighbors_angular(possible_neighbors: np.ndarray,
             return np.array([[np.nan, np.nan, np.nan, np.nan, np.nan]])
 
 
-if __name__ == '__main__':
-    import datetime
-    unknown_positions = np.random.random(size=(1000, 2))
-    known_positions = np.random.random(size=(5000, 3))
-
-    t0 = datetime.datetime.now()
-    for uk in unknown_positions:
-        _ = select_kriging_data_from_direction(
-            unknown_position=uk,
-            data_array=known_positions,
-            neighbors_range=0.1,
-            direction=15,
-            number_of_neighbors=4,
-            max_tick=5
-        )
-    tx = datetime.datetime.now() - t0
-    print(tx.total_seconds())
+# if __name__ == '__main__':
+#     import datetime
+#
+#     unknown_positions = np.random.random(size=(1000, 2))
+#     known_positions = np.random.random(size=(5000, 3))
+#
+#     t0 = datetime.datetime.now()
+#     for uk in unknown_positions:
+#         _ = select_kriging_data_from_direction(
+#             unknown_position=uk,
+#             data_array=known_positions,
+#             neighbors_range=0.1,
+#             direction=15,
+#             number_of_neighbors=4,
+#             max_tick=5
+#         )
+#     tx = datetime.datetime.now() - t0
+#     print(tx.total_seconds())
