790c7d2e91aa57ea705117b7474d80c857bcde43
Simon <31246246+SimonMolinsky@users.noreply.github.com>
2025-05-02 18:39:08 +0300

    updated kriging benchmarking tutorial

--- src/pyinterpolate/idw/idw.py
+++ src/pyinterpolate/idw/idw.py
@@ -11,12 +11,14 @@ TODO
 from typing import Iterable
 
 import numpy as np
+
+from pyinterpolate.core.data_models.points import VariogramPoints
 from pyinterpolate.distance.point import point_distance
 
 
-def inverse_distance_weighting(known_points: np.ndarray,
+def inverse_distance_weighting(known_locations: np.ndarray,
                                unknown_location: Iterable,
-                               number_of_neighbours=-1,
+                               no_neighbors=-1,
                                power=2.) -> float:
     """
     Inverse Distance Weighting with a given set of points and
@@ -24,7 +26,7 @@ def inverse_distance_weighting(known_points: np.ndarray,
 
     Parameters
     ----------
-    known_points : numpy array
+    known_locations : numpy array
         The MxN array, where **M** is a number of rows (points) and **N**
         is the number of columns, where the last column represents a value
         observed in a known point. (It could be **(N-1)**-dimensional data).
@@ -37,7 +39,7 @@ def inverse_distance_weighting(known_points: np.ndarray,
         is added once - vector of points ``[x, y]``
         becomes ``[[x, y]]`` for 2-dimensional data.
 
-    number_of_neighbours : int, default = -1
+    no_neighbors : int, default = -1
         If default value **(-1)** then all known points will be used to
         estimate value at the unknown location.
         Can be any number within the limits ``[2, len(known_points)]``,
@@ -66,44 +68,48 @@ def inverse_distance_weighting(known_points: np.ndarray,
     if power < 0:
         raise ValueError('Power cannot be smaller than 0')
 
+    # Check known points parameter
+    # Check if known locations are in the right format
+    known_locations = VariogramPoints(known_locations).points
+
     # Check number of neighbours parameter
-    nn_neighbors_ge_2 = number_of_neighbours >= 2
-    nn_neighbors_le_known_points = number_of_neighbours <= len(known_points)
+    nn_neighbors_ge_2 = no_neighbors >= 2
+    nn_neighbors_le_known_points = no_neighbors <= len(known_locations)
     n_closest_eq_nn = nn_neighbors_ge_2 and nn_neighbors_le_known_points
 
-    number_of_closest = len(known_points)
+    number_of_closest = len(known_locations)
     
-    if number_of_neighbours == -1:
+    if no_neighbors == -1:
         pass
     elif n_closest_eq_nn:
-        number_of_closest = number_of_neighbours
+        number_of_closest = no_neighbors
     else:
-        _idw_value_error_nn(length_known=len(known_points),
-                            nn=number_of_neighbours)
+        _idw_value_error_nn(length_known=len(known_locations),
+                            nn=no_neighbors)
 
     # Pre-process unknown location parameter
     if not isinstance(unknown_location, np.ndarray):
         unknown_location = np.array(unknown_location)
 
-    if len(unknown_location.shape) != len(known_points.shape):
+    if len(unknown_location.shape) != len(known_locations.shape):
         unknown_location = unknown_location[np.newaxis, ...]
 
     # Calculate distances
-    distances = point_distance(unknown_location, known_points[:, :-1])
+    distances = point_distance(unknown_location, known_locations[:, :-1])
     distances: np.ndarray
 
     # Check if any distance is equal to 0 - then return this value
     if not np.all(distances[0]):
 
         zer_pos = np.where(distances == 0)
-        result = known_points[zer_pos[1], -1][0]
+        result = known_locations[zer_pos[1], -1][0]
         return result
 
     # Get n closest neighbours...
     sdists = distances.argsort()
     sdists = sdists[0, :number_of_closest]
     dists = distances[0, sdists]
-    values = known_points[sdists].copy()
+    values = known_locations[sdists].copy()
     values = values[:, -1]
 
     # Create weights
